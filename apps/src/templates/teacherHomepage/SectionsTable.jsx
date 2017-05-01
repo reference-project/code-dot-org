040e29d4587c585b6c15d0ff6bf1cbe13976d486
Erin Bond <erin.bond@code.org>
2017-05-01 16:52:45 -0700

    Initial work on SectionsTable - needs styling, links and sortability

--- apps/src/templates/teacherHomepage/SectionsTable.jsx
+++ apps/src/templates/teacherHomepage/SectionsTable.jsx
@@ -1,15 +1,126 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import color from "@cdo/apps/util/color";
+import i18n from '@cdo/locale';
+
+const styles = {
+  table: {
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: color.border_gray,
+    borderRadius: 3
+  },
+  headerRow: {
+    backgroundColor: color.table_header,
+    fontWeight: 'bold',
+    borderBottomColor: color.border_light_gray,
+    borderBottomWidth: 1,
+    borderBottomStyle: 'solid',
+  },
+  col1: {
+    borderRightWidth: 1,
+    borderRightColor: color.border_light_gray,
+    borderRightStyle: 'solid',
+    lineHeight: '52px',
+    color: color.charcoal,
+    paddingLeft: 20,
+    paddingRight: 20,
+    minWidth: 300
+  },
+  col2: {
+    borderRightWidth: 1,
+    borderRightColor: color.border_light_gray,
+    borderRightStyle: 'solid',
+    lineHeight: '52px',
+    color: color.charcoal,
+    paddingLeft: 20,
+    paddingRight: 20,
+    minWidth: 300
+  },
+  col3: {
+    borderRightWidth: 1,
+    borderRightColor: color.border_light_gray,
+    borderRightStyle: 'solid',
+    lineHeight: '52px',
+    color: color.charcoal,
+    paddingLeft: 20,
+    paddingRight: 20,
+    minWidth: 100
+  },
+  col4: {
+    lineHeight: '52px',
+    color: color.charcoal,
+    paddingLeft: 20,
+    paddingRight: 20,
+    minWidth: 200
+  },
+  colText: {
+    color: color.charcoal,
+    fontFamily: '"Gotham 5r", sans-serif',
+    fontSize: 14,
+  },
+};
 
 const SectionsTable = React.createClass({
   propTypes: {
-
+    sections: PropTypes.array.isRequired,
   },
 
   render() {
+    const { sections } = this.props;
+
     return (
-      <div>
-        This is where the Sections Table will be!
-      </div>
+      <table style={styles.table}>
+        <thead>
+          <tr style={styles.headerRow}>
+            <td style={styles.col1}>
+              <div style={styles.colText}>
+                {i18n.section()}
+              </div>
+            </td>
+            <td style={styles.col2}>
+              <div style={styles.colText}>
+                {i18n.course()}
+              </div>
+            </td>
+            <td style={styles.col3}>
+              <div style={styles.colText}>
+                {i18n.students()}
+              </div>
+            </td>
+            <td style={styles.col4}>
+              <div style={styles.colText}>
+                {i18n.sectionCode()}
+              </div>
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          {sections.map((section, index) =>
+            <tr style={styles.row} key={index}>
+              <td style={styles.col1}>
+                <div style={styles.colText}>
+                  {section.name}
+                </div>
+              </td>
+              <td style={styles.col2}>
+                <div style={styles.colText}>
+                  {section.course}
+                </div>
+              </td>
+              <td style={styles.col3}>
+                <div style={styles.colText}>
+                  {section.students.length}
+                </div>
+              </td>
+              <td style={styles.col4}>
+                <div style={styles.colText}>
+                  {section.sectionCode}
+                </div>
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     );
   }
 });
--- apps/src/templates/teacherHomepage/SectionsTable.story.jsx
+++ apps/src/templates/teacherHomepage/SectionsTable.story.jsx
@@ -9,7 +9,23 @@ export default storybook => {
         name: 'Section Table - one section',
         description: 'This is an example of a very simple Section Table with the teacher has one section',
         story: () => (
-          <SectionsTable/>
+          <SectionsTable
+            sections={[
+              {
+                name: "Algebra Period 1",
+                course: "CS in Algebra",
+                students: ['Abby', 'Beth'],
+                sectionCode: "ABC123"
+              },
+              {
+                name: "Period 3",
+                course: "Course 2",
+                students: ['Abby', 'Beth', 'Carol'],
+                sectionCode: "ABC123"
+              },
+
+            ]}
+          />
         )
       },
     ]);
